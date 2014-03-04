7495ffbadcccfd5cd0d3f323b8c9237dca54197a
Thwaraka Yogarajah <thuwa_14d@yahoo.com>
2014-03-04 19:13:47 +0530

    Binary Tree Test Console

---
+++ DSA/src/DSA/BinaryTreeTest.java
@@ -0,0 +1,22 @@
+package BSTree;
+
+public class BinaryTreeTest {
+
+
+    
+    public void test(){
+        
+        BinaryTree tree = new BinaryTree();
+        
+        tree.add(50);
+        tree.add(70);
+        
+        
+                     
+        System.out.println("");
+        
+        
+    }
+	
+	
+}
