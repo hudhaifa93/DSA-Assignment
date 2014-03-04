02bce7027c86e2ad87ff25c1647be88b4530171b
Gowthamaravi <gowthammailvaganam@gmail.com>
2014-03-04 19:30:17 +0530

    Delete Node

--- DSA/src/DSA/BinaryTree.java
+++ DSA/src/DSA/BinaryTree.java
@@ -99,7 +99,163 @@ public class BinaryTree {
 	        }
 	    }
 		
-		
+		public boolean delete(int val) {
+
+	        //case1 node has no children
+	        //case2 node has one child
+	        //case3 node has two child
+	        Node nodeToBeDeleted = find(val);
+	        
+	        
+	        
+	        if (nodeToBeDeleted != null) {
+	        	
+	        	if(root.isbn == nodeToBeDeleted.isbn){
+	        		
+		        	deleteCase4(nodeToBeDeleted);
+		        	
+		        	
+		        }else{
+		        	
+		        	if (nodeToBeDeleted.leftChild == null && nodeToBeDeleted.rightChild == null) {
+
+	                //case1
+	                deleteCase1(nodeToBeDeleted);
+	                
+	            } 
+	            if (nodeToBeDeleted.leftChild != null && nodeToBeDeleted.rightChild != null) {
+
+	                //case2 left child should be deleted
+	                deleteCase3(nodeToBeDeleted);
+	                
+	            } else if (nodeToBeDeleted.leftChild != null ) {
+
+	                //case2 right child should be deleted
+	                deleteCase2(nodeToBeDeleted);
+	                
+	            }else if( nodeToBeDeleted.rightChild != null) {
+	            	
+	            	deleteCase2(nodeToBeDeleted);
+	            }
+		        	
+		        }
+	            
+	            
+	            
+	        }
+	        
+	        
+	        
+	        return false;
+	    }
+	    
+	    
+	    // case 4 if the nodeToBeDeleteed is the tree root
+	    private void deleteCase4(Node nodeToBeDeleted) {
+			
+	    	Node minNode = minLeftTraversal(nodeToBeDeleted.rightChild);
+	    	
+	    	root.isbn = minNode.isbn;
+	    	root.auther = minNode.auther;
+	    	root.title = minNode.title;
+	    	root.surname = minNode.surname;
+	    	
+	    	if(minNode.rightChild == null){
+	    		
+	    		deleteCase1(minNode);
+	    		
+	    	}else{
+	    		
+	    		deleteCase2(minNode);
+	    	}
+			
+		}
+
+		private void deleteCase1(Node nodeToBeDeleted) {
+
+	        //check if node to be deleted is the left or right CHILD of the PARENT of the node 
+			if(nodeToBeDeleted.parent != null){
+				if (nodeToBeDeleted.parent.leftChild == nodeToBeDeleted) {
+	            
+	            nodeToBeDeleted.parent.leftChild = null;
+	            
+	        } else if (nodeToBeDeleted.parent.rightChild == nodeToBeDeleted) {
+	            
+	            nodeToBeDeleted.parent.rightChild = null;
+	            
+	        }
+			}
+	        
+	    }
+	    
+	    private void deleteCase2(Node nodeToBeDeleted) {
+	        
+	        if (nodeToBeDeleted.parent.leftChild == nodeToBeDeleted) {
+	            
+	            if (nodeToBeDeleted.leftChild != null) {
+	                
+	                nodeToBeDeleted.parent.leftChild = nodeToBeDeleted.leftChild;
+	                
+	            } else if (nodeToBeDeleted.rightChild != null) {
+	                
+	                nodeToBeDeleted.parent.leftChild = nodeToBeDeleted.rightChild;
+	                
+	            }
+	            
+	        } else if (nodeToBeDeleted.parent.rightChild == nodeToBeDeleted) {
+	            
+	            if (nodeToBeDeleted.leftChild != null) {
+	                
+	                nodeToBeDeleted.parent.rightChild = nodeToBeDeleted.leftChild;
+	                
+	            } else if (nodeToBeDeleted.rightChild != null) {
+	                
+	                nodeToBeDeleted.parent.rightChild = nodeToBeDeleted.rightChild;
+	                
+	            }
+	            
+	        }
+	        
+	    }
+	    
+	    private void deleteCase3(Node nodeToBeDeleted) {
+	        
+	        Node minNode = minLeftTraversal(nodeToBeDeleted.rightChild);
+	        
+	        if(minNode.rightChild == null)
+	        	deleteCase1(minNode);
+	        else
+	        	deleteCase2(minNode);
+	        
+	        minNode.parent = nodeToBeDeleted.parent;
+	        
+	        minNode.leftChild = nodeToBeDeleted.leftChild;
+	        
+	        minNode.rightChild = nodeToBeDeleted.rightChild;
+	        
+	        if (nodeToBeDeleted.parent.leftChild == nodeToBeDeleted) {
+	            
+	            nodeToBeDeleted.parent.leftChild = minNode;
+	            
+	        } else if (nodeToBeDeleted.parent.rightChild == nodeToBeDeleted) {
+	            
+	            nodeToBeDeleted.parent.rightChild = minNode;
+	            
+	        }
+	        
+	    }
+	    
+	    private Node minLeftTraversal(Node node) {
+	        
+	        if (node.leftChild == null) {
+	            
+	            return node;
+	            
+	        }
+	        
+	        return minLeftTraversal(node.leftChild);
+	        
+	    }
 	    
 	    
 	    
